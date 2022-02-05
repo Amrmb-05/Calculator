0cb8360cc9231037654a58f9deb4b2973588523f
Amr Mohamed <amoree.mb@gmail.com>
2022-02-06 00:09:43 +0200

    calculator now fuly functional

--- script.js
+++ script.js
@@ -20,9 +20,63 @@ function operate(a, b, oper) {
             return add(a,b);
         case '-':
             return subtract(a, b);
-        case '*':
+        case 'x':
             return multiply(a, b);
         case '/':
             return divide(a, b);
     }
 }
+let display = document.getElementById('display-text');
+numButtons = document.querySelectorAll('.numBtns');
+console.log(numButtons)
+
+let displayValue = []
+numButtons.forEach( button => {button.addEventListener('click',() => {
+    if (displayValue.length ===1 || displayValue.length === 0) {
+        display.textContent += button.textContent
+        displayValue[0] = parseInt(display.textContent)
+        
+    }
+    else if (displayValue.length != 1 && displayValue.length != 0) {//|| displayValue.length === 3) {
+        if (!isNaN(displayValue[2])){
+        display.textContent += button.textContent
+        displayValue[2] = parseInt(display.textContent)
+        }
+        else {
+            display.textContent = button.textContent
+            displayValue[2] = parseInt(display.textContent)
+        }
+    }
+    console.log(displayValue)
+})})
+
+operatorBtns = document.querySelectorAll('.operators');
+operatorBtns.forEach( button => {button.addEventListener('click',() =>{
+    display.textContent = ''
+    if(displayValue.length == 3) {
+        display.textContent = operate(displayValue[0],displayValue[2],displayValue[1])
+        displayValue.splice(0,3)
+        displayValue[0] = parseInt(display.textContent)
+        console.log('amr')
+    }
+    displayValue[1] = button.textContent
+    
+    
+})
+})
+
+equalBtn = document.querySelector('#equal');
+equalBtn.addEventListener('click', () => {
+    if(displayValue.length < 3) {
+        display.textContent = ''
+        displayValue.splice(0,3)
+    }
+    display.textContent = operate(displayValue[0],displayValue[2],displayValue[1])
+    displayValue[0] = parseInt(display.textContent)
+})
+
+clearBtn = document.querySelector('.clear');
+clearBtn.addEventListener('click', () => {
+    display.textContent = ''
+    displayValue.splice(0,3)
+})
